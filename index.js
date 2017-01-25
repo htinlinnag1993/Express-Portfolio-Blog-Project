18a4406e45e7519fa5d4da278300f25501307a1a
Htin Linn Aung <htinlinnag1993@gmail.com>
2017-01-24 22:48:37 -0500

    changes 1

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-//export POSTGRES_USER=postgres
-//export POSTGRES_PASSWORD=TTPsuccess3*
-// postgres:TTPsuccess3*
-
-// must install express, pg, body-parser, ejs,
-
+// must install node, express, pg, body-parser, ejs to use below features
 var express = require('express'); //acquire express library folder
 var pg = require('pg'); //acquire a server and pg library folder // pg is the database adaptor
 var bodyParser = require('body-parser'); //acquire body-parser library folder
 
 var app = express(); //just to use only one function 'express()', which is express.js in the express folder, everytime app is called
+
+var port = process.env.PORT || 3000; //for setting up database in Heroku database instead of using database in my machine
 
 app.set('view engine', 'ejs'); //setting view engine for template using ejs
 app.set('views', './views');
@@ -18,61 +15,80 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: false}));
 // app.use(bodyParser());
 
-var dbURL = 'postgres://'+ process.env.POSTGRES_USER +':'+ process.env.POSTGRES_PASSWORD +'@localhost:5432/bulletinb'; //setting environment variable. You could also do this in your system setting
-
+// For Home page(landing page)
 app.get('/', function(req, res){
-  pg.connect(dbURL, function(err, client, done){
+
+})
+
+// For About page
+app.get('/about', function(req, res){
+
+})
+
+// For Projects page
+app.get('/projects', function(req, res){
+
+})
+
+// For Blog Home page
+app.get('/blog', function(req, res){
+  // below is for setting up database in Heroku database instead of using database in my machine
+  // res.send("Hello");
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err){
-      return console.log("errorConnecting");
+      return console.log(err);
     }
-    client.query(`select * from bloglogs`, function(err, result){
+    client.query(`select * from messages`, function(err, result){
       if (err){
-        return console.log("errorGettingMessages");
+        return console.log(err);
       }
       res.render("blog_home", result);
       done();
-      pg.end();
+      // pg.end();
     });
   });
 });
 
-app.get('/blog_board', function(req, res){
-  pg.connect(dbURL, function(err, client, done){
+// For Blog Articles Board page
+app.get('/blog/content', function(req, res){
+  // below is for setting up database in Heroku database instead of using database in my machine
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err){
-      return console.log("errorConnecting");
+      return console.log(err);
     }
-    client.query(`select * from bloglogs`, function(err, result){
+    client.query(`select * from messages`, function(err, result){
       if (err){
-        return console.log("errorGettingMessages");
+        return console.log(err);
       }
       res.render("blog_board", result);
       done();
-      pg.end();
+      // pg.end();
     });
   });
 });
 
-app.get("/write", function(req, res){
-  res.redirect("/");
-});
+// app.get("/write", function(req, res){
+//   res.redirect("/");
+// });
 
-app.post("/write", function(req, res){
-  pg.connect(dbURL, function(err, client, done){
+// For posting on the Blog Articles Board (Entries into Database)
+app.post("/blog/content", function(req, res){
+  // below is for setting up database in Heroku database instead of using database in my machine
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     // console.log("before client.query");
     if (err){
-      return console.log("errorPosting");
+      return console.log(err);
     }
-    client.query("insert into bloglogs (title, body, date) values ('" +req.body.title + "','" + req.body.body + "','" + req.body.date + "');", function(err,result){
-      //(`insert into messages (email, title, body) values ('`+ req.body.email + `','` + req.body.title + `','` + req.body.body +`');`
-      // values ('${req.body.email}','${req.body.title}','${req.body.body}');`
+    client.query(`insert into messages (title, body, date) values ('${req.body.title}', '${req.body.body}', '${req.body.date}')`, function(err,result){
       if (err){
-        return console.log("errorWriting");
+        return console.log("err");
       }
-      res.redirect("/blog_board");
+      res.redirect("/blog/content");
     });
   });
 });
 
-app.listen(3000, function(){
+// app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Listening on port_3000");
 });
